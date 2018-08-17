60d05cd34a9c7f180a8aa07fa0df51ba048e8651
Jacky Wu <jackywu567@gmail.com>
2018-08-17 14:41:34 +0800

    new file:   06_RPC/RpcClient/.vs/RpcClient/DesignTimeBuild/.dtbcache 	new file:   06_RPC/RpcClient/.vs/RpcClient/v15/.suo 	new file:   06_RPC/RpcClient/.vs/RpcClient/v15/Server/sqlite3/db.lock 	new file:   06_RPC/RpcClient/.vs/RpcClient/v15/Server/sqlite3/storage.ide 	new file:   06_RPC/RpcClient/.vs/RpcClient/v15/Server/sqlite3/storage.ide-shm 	new file:   06_RPC/RpcClient/.vs/RpcClient/v15/Server/sqlite3/storage.ide-wal 	new file:   06_RPC/RpcClient/Program.cs 	new file:   06_RPC/RpcClient/Properties/launchSettings.json 	new file:   06_RPC/RpcClient/RpcClient.cs 	new file:   06_RPC/RpcClient/RpcClient.csproj 	new file:   06_RPC/RpcClient/RpcClient.sln 	new file:   06_RPC/RpcClient/obj/RpcClient.csproj.nuget.cache 	new file:   06_RPC/RpcClient/obj/RpcClient.csproj.nuget.g.props 	new file:   06_RPC/RpcClient/obj/RpcClient.csproj.nuget.g.targets 	new file:   06_RPC/RpcClient/obj/project.assets.json 	new file:   06_RPC/RpcServer/.vs/RpcServer/DesignTimeBuild/.dtbcache 	new file:   06_RPC/RpcServer/.vs/RpcServer/v15/.suo 	new file:   06_RPC/RpcServer/.vs/RpcServer/v15/Server/sqlite3/db.lock 	new file:   06_RPC/RpcServer/.vs/RpcServer/v15/Server/sqlite3/storage.ide 	new file:   06_RPC/RpcServer/.vs/RpcServer/v15/Server/sqlite3/storage.ide-shm 	new file:   06_RPC/RpcServer/.vs/RpcServer/v15/Server/sqlite3/storage.ide-wal 	new file:   06_RPC/RpcServer/Program.cs 	new file:   06_RPC/RpcServer/RpcServer.csproj 	new file:   06_RPC/RpcServer/RpcServer.sln 	new file:   06_RPC/RpcServer/obj/RpcServer.csproj.nuget.cache 	new file:   06_RPC/RpcServer/obj/RpcServer.csproj.nuget.g.props 	new file:   06_RPC/RpcServer/obj/RpcServer.csproj.nuget.g.targets 	new file:   06_RPC/RpcServer/obj/project.assets.json

---
+++ Tutorials/06_RPC/RpcClient/RpcClient.cs
@@ -0,0 +1,73 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RpcClient
+{
+    public class RpcClient
+    {
+        private readonly IConnection connection;
+        private readonly IModel channel;
+        private readonly string replyQueueName;
+        private readonly EventingBasicConsumer consumer;
+
+        public RpcClient()
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            connection = factory.CreateConnection();
+            channel = connection.CreateModel();
+            replyQueueName = channel.QueueDeclare().QueueName;
+            consumer = new EventingBasicConsumer(channel);
+        }
+
+        public string Call(string message)
+        {
+            var tcs = new TaskCompletionSource<string>();
+            var resultTask = tcs.Task;
+
+            var correlationId = Guid.NewGuid().ToString();
+
+            var props = channel.CreateBasicProperties();
+            props.ReplyTo = replyQueueName;
+            props.CorrelationId = correlationId;
+
+            EventHandler<BasicDeliverEventArgs> handler = null;
+            handler = (model, ea) =>
+            {
+                if (ea.BasicProperties.CorrelationId == correlationId)
+                {
+                    consumer.Received -= handler;
+
+                    var body = ea.Body;
+                    var response = Encoding.UTF8.GetString(body);
+
+                    tcs.SetResult(response);
+                }
+            };
+
+            consumer.Received += handler;
+
+            var messageBytes = Encoding.UTF8.GetBytes(message);
+            channel.BasicPublish(
+            exchange: "",
+            routingKey: "rpc_queue",
+            basicProperties: props,
+            body: messageBytes);
+
+            channel.BasicConsume(
+            consumer: consumer,
+            queue: replyQueueName,
+            autoAck: true);
+
+            return resultTask.Result;
+        }
+
+        public void Close()
+        {
+            connection.Close();
+        }
+    }
+}
---
+++ Tutorials/06_RPC/RpcServer/Program.cs
@@ -0,0 +1,71 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+
+namespace RpcServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { };
+
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare(queue: "rpc_queue", durable: false, exclusive: false, autoDelete: false, arguments: null);
+                    channel.BasicQos(0, 1, false);
+
+                    var consumer = new EventingBasicConsumer(channel);
+                    channel.BasicConsume(queue: "rpc_queue", autoAck: false, consumer: consumer);
+                    Console.WriteLine(" [x] Awaiting RPC requests");
+
+                    consumer.Received += (model, eventArgs) => 
+                    {
+                        string response = null;
+
+                        var body = eventArgs.Body;
+                        var replyTo = eventArgs.BasicProperties.ReplyTo;
+
+                        var repProps = channel.CreateBasicProperties();
+                        repProps.CorrelationId = eventArgs.BasicProperties.CorrelationId;
+
+                        try
+                        {
+                            var message = Encoding.UTF8.GetString(body);
+                            int n = int.Parse(message);
+                            Console.WriteLine(" [.] fib({0})", message);
+                            response = fib(n).ToString();
+                        }
+                        catch (Exception e)
+                        {
+                            Console.WriteLine(" [.] " + e.Message);
+                            response = "";
+                        }
+                        finally
+                        {
+                            var responseBytes= Encoding.UTF8.GetBytes(response);
+                            channel.BasicPublish(exchange: "", routingKey: replyTo, basicProperties: repProps, body: responseBytes);
+                            channel.BasicAck(deliveryTag: eventArgs.DeliveryTag, multiple: false);
+                        }
+                    };
+
+                    Console.WriteLine(" Press [enter] to exit.");
+                    Console.ReadLine();
+                }
+            }
+        }
+
+        private static int fib(int n)
+        {
+            if (n == 0 || n == 1)
+            {
+                return n;
+            }
+
+            return fib(n - 1) + fib(n - 2);
+        }
+    }
+}
